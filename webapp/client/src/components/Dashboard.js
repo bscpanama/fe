23b122004d8a7a0d4ab0348d702f34c07833bd50
Frank Ojeda E <frankplay19@gmail.com>
2019-03-10 20:53:58 -0500

    Changes on the dashboard

--- webapp/client/src/components/Dashboard.js
+++ webapp/client/src/components/Dashboard.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import requireAuth from './requireAuth';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import { Bar } from 'react-chartjs-2';
 
+import { loadStats } from '../actions/index.js';
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+
+         this.state = {
+            query: 'monthly'   
+        }
+    }
+
 	componentDidMount() {
 	    document.title = "BS&C - Dashboard";
 	    document.body.classList.remove('login');
+
+        this.props.loadStats(this.state.query);
 	}
 
   render(){
+    const chartData = {
+            labels: Object.keys(this.props.stats),
+            datasets: [{
+                label: 'Facturas',
+                data: Object.values(this.props.stats),
+                backgroundColor: 'rgb(15, 128, 103)',
+                borderWidth: 0
+            }]
+        };
+    const chartOptions = {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        };
     return (
     	<div>
     	<Sidebar />
@@ -18,6 +50,12 @@ class Dashboard extends Component {
     		<Header/>             
     		<div className="main-container">
             <div className="clearfix">
+                <div className="stats-wrapper dashstats">
+                    <div className="stats-options">
+                        <div className="title">Estadísticas Mensuales</div>
+                    </div>
+                    <Bar data={chartData} options={chartOptions} height={50} />
+                </div>
                 <div className="activity-table">
                     <div className="t-header">
                         <div className="t-col">Últimos Usuarios</div>
@@ -52,4 +90,13 @@ class Dashboard extends Component {
   }
 }
 
-export default requireAuth(Dashboard);
+function mapStateToProps({ movements }) {
+    const { stats } = movements;
+    return { stats };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ loadStats }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(requireAuth(Dashboard));
